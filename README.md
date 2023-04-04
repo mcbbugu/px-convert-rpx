dc7941750294f51993112f50c93ed9281c7660e2
mcbbugu <1443786299@qq.com>
2023-04-04 19:56:39 +0800

    feat: first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# px-convert-rpx
-.css 和 .vue 文件中，px 和 rpx的相互转换
+# pxtotorpx README
+
+.css 和 .vue 文件中，px 和 rpx的相互转换，支持整个文件夹的转换。
+
+## Features
+
+安装后，在文件夹上右键选择相应操作，在打开的文件中，右键选择相应操作。
+可在插件设置中修改设计稿宽度。
---
+++ extension.js
@@ -0,0 +1,177 @@
+const { log } = require('console');
+const vscode = require('vscode');
+
+function activate(context) {
+
+	let disposable = vscode.commands.registerTextEditorCommand('extension.convertSelectedPxToRpx', (textEditor, edit) => {
+		const selection = textEditor.selection;
+		const document = textEditor.document;
+		let text;
+
+		if (!selection.isEmpty) {
+			text = document.getText(selection);
+		} else {
+			text = document.getText();
+		}
+
+		const textWithRpx = convertPxToRpx(text)
+
+		if (!selection.isEmpty) {
+			edit.replace(selection, textWithRpx);
+		} else {
+			const lastLine = document.lineAt(document.lineCount - 1);
+			const selection = new vscode.Selection(new vscode.Position(0, 0), new vscode.Position(lastLine.lineNumber, lastLine.text.length));
+			textEditor.edit(edit => {
+				edit.replace(selection, textWithRpx);
+			});
+		}
+	});
+
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerTextEditorCommand('extension.convertSelectedRpxToPx', (textEditor, edit) => {
+		const selection = textEditor.selection;
+		const document = textEditor.document;
+		let text;
+
+		if (!selection.isEmpty) {
+			text = document.getText(selection);
+		} else {
+			text = document.getText();
+		}
+
+		const textWithPx = convertRpxToPx(text)
+
+		if (!selection.isEmpty) {
+			edit.replace(selection, textWithPx);
+		} else {
+			const lastLine = document.lineAt(document.lineCount - 1);
+			const selection = new vscode.Selection(new vscode.Position(0, 0), new vscode.Position(lastLine.lineNumber, lastLine.text.length));
+			textEditor.edit(edit => {
+				edit.replace(selection, textWithPx);
+			});
+		}
+	});
+
+	context.subscriptions.push(disposable);
+
+	// px to rpx
+	function convertPxToRpx(fileText) {
+		const configuration = vscode.workspace.getConfiguration('pxtotorpx');
+		const designWidth = configuration.designWidth;
+		console.log(`designWidth: ${designWidth}`);
+		const rePx = /(\d+(?:\.\d+)?)px/g;
+		const textWithRpx = fileText.replace(rePx, function ($0, $1) {
+			const pxValue = parseFloat($1);
+			const rpxValue = 750 * pxValue / designWidth;
+			return `${rpxValue.toFixed(2)}rpx`;
+		});
+
+		return textWithRpx;
+	}
+
+	// rpx to px
+	function convertRpxToPx(fileText) {
+		const configuration = vscode.workspace.getConfiguration('pxtotorpx');
+		const designWidth = configuration.designWidth;
+		console.log(`designWidth: ${designWidth}`);
+		const reRpx = /(\d+(?:\.\d+)?)rpx/g; // 修改正则匹配规则
+
+		const textWithPx = fileText.replace(reRpx, function ($0, $1) { // 将 px 替换为 rpx
+			const rpxValue = parseFloat($1);
+			const pxValue = rpxValue * designWidth / 750;
+			return `${pxValue.toFixed(0)}px`;
+		});
+
+		return textWithPx;
+	}
+
+	const convertFolderCommand = vscode.commands.registerCommand('extension.convertFolder', (folderUri) => {
+		if (folderUri) {
+			const folderPath = folderUri.path; // 获取文件夹路径
+			vscode.workspace.findFiles(new vscode.RelativePattern(folderPath, "*.{vue}")).then((files) => {
+				files.forEach((fileUri) => {
+					const fileContent = vscode.workspace.fs.readFile(fileUri).then((content) => {
+						const fileText = content.toString();
+						const convertedText = convertPxToRpx(fileText); // 调用单个文件转换代码
+
+						vscode.workspace.fs.writeFile(fileUri, Buffer.from(convertedText));
+					});
+				});
+				vscode.window.showInformationMessage(`Successfully convert ${files.length} files! px to rpx`);
+			});
+		}
+	});
+	context.subscriptions.push(convertFolderCommand);
+
+
+	const convertFolderCommandRpxToPx = vscode.commands.registerCommand('extension.convertFolderRpxToPx', (folderUri) => {
+		if (folderUri) {
+			const folderPath = folderUri.path; // 获取文件夹路径
+			vscode.workspace.findFiles(new vscode.RelativePattern(folderPath, "*.{vue}")).then((files) => {
+				files.forEach((fileUri) => {
+					const fileContent = vscode.workspace.fs.readFile(fileUri).then((content) => {
+						const fileText = content.toString();
+						const convertedText = convertRpxToPx(fileText); // 调用单个文件转换代码
+
+						vscode.workspace.fs.writeFile(fileUri, Buffer.from(convertedText));
+					});
+				});
+				vscode.window.showInformationMessage(`Successfully convert ${files.length} files! rpx to px`);
+			});
+		}
+	});
+	context.subscriptions.push(convertFolderCommandRpxToPx);
+
+
+
+
+	async function convertFolder(folderPath, type) {
+		try {
+			const cssPattern = '**/src/**/*.css';
+    const vuePattern = '**/src/**/*.vue';
+			const cssFiles = await vscode.workspace.findFiles(cssPattern, folderPath);
+			const vueFiles = await vscode.workspace.findFiles(vuePattern, folderPath);
+			const filesToConvert = [...cssFiles, ...vueFiles];
+			for (const file of filesToConvert) {
+				const document = await vscode.workspace.openTextDocument(file);
+				await convertDocument(document, type);
+			}
+			vscode.window.showInformationMessage(`文件夹下的所有文件中 px 单位已经成功转换为 rpx 单位。`);
+		} catch (error) {
+			console.error(error);
+			vscode.window.showErrorMessage(`转换过程中发生错误：${error.message}`);
+		}
+	}
+
+	async function convertDocument(document, type) {
+		const originalContent = document.getText();
+		let convertedContent = ''
+		if (type === 1) {
+			convertedContent = convertPxToRpx(originalContent);
+		} else {
+			convertedContent = convertRpxToPx(originalContent);
+		}
+		if (originalContent !== convertedContent) {
+			const edit = new vscode.WorkspaceEdit();
+			const start = new vscode.Position(0, 0);
+			const end = document.lineAt(document.lineCount - 1).range.end;
+			const wholeRange = new vscode.Range(start, end);
+			edit.replace(document.uri, wholeRange, convertedContent);
+			await vscode.workspace.applyEdit(edit);
+		}
+	}
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.convertPxToRpxInFolder', async (uri) => {
+		const folderPath = uri.fsPath;
+		convertFolder(folderPath, 1)
+	}));
+
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.convertRpxToPxInFolder', async (uri) => {
+		const folderPath = uri.fsPath;
+		convertFolder(folderPath, 2)
+	}));
+}
+
+exports.activate = activate;
